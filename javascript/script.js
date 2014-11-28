d8f28c9edd8632deac5fe279c912b1cf7a1693cf
Vezel <Gustav@Gustavs-MacBook-Air.local>
2014-11-28 16:47:23 +0100

    start

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+var spelplan,
+    ctx;
+
+var monsterHealth = 10,
+    dmg = 1,
+    
+
+function start() {
+    spelplan = document.getElementById("spelplan");
+    ctx = spelplan.getContext("2d");
+    
+    window.setInterval(update, 25);
+}
+
+function mouseHandler() {
+}
+
+function update() {
+    
+}
